ec1d99cb68c52a529656c03beb5747a439d4bdf6
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-24 11:31:48 +0900

    fix: フォルダ名の修正

--- app/controllers/users/issues_controller.rb
+++ app/controllers/users/issues_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User::IssuesController < ApplicationController
+class Users::IssuesController < ApplicationController
   def index
     @repository = Repository.find_by(name: ENV['REPOSITORY_NAME'])
     @user = User.find_by(login: params[:user_login])
