a212e545afea360d21af0928e60369bed2b5b8b4
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-26 20:28:44 +0900

    refactor: ユーザーの Issue / Wiki の一覧画面にパーシャルを導入

--- app/controllers/users/wikis_controller.rb
+++ app/controllers/users/wikis_controller.rb
@@ -4,5 +4,6 @@ class Users::WikisController < ApplicationController
   def index
     @repository = Repository.find_by(name: ENV['REPOSITORY_NAME'])
     @user = User.find_by(login: params[:user_login])
+    @wikis = @user.wikis.order(:created_at)
   end
 end
