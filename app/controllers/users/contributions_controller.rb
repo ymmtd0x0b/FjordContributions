0f68b5af4c1db76f2b9694603a6c06bb161dafb1
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-28 01:41:03 +0900

    refector: コントローラの重複処理を共通化

--- app/controllers/users/contributions_controller.rb
+++ app/controllers/users/contributions_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class Users::ContributionsController < ApplicationController
+  include Settable
+  before_action :set_repository, only: %i[index]
+  before_action :set_user, only: %i[index]
+
   def index
-    @repository = Repository.find_by(name: ENV['REPOSITORY_NAME'])
-    @user = User.find_by(login: params[:user_login])
     @issues = @user.issues.order(:created_at)
     @assigned_issues = @user.assigned_issues.order(:created_at)
     @reviewed_issues = @user.reviewed_issues.order(:created_at)
