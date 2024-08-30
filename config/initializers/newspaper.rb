f307c8cd021fe9b73c1254b3292dcefe5354f457
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-31 08:19:09 +0900

    feat: アカウント登録時に「ユーザーがレビューした PullRequest を取得・登録する機能を追加

--- config/initializers/newspaper.rb
+++ config/initializers/newspaper.rb
@@ -3,4 +3,5 @@ Rails.configuration.after_initialize do
   Newspaper.subscribe(:create_user, Synchronizer::AssignedIssue.new)
   Newspaper.subscribe(:create_user, Synchronizer::ReviewedIssue.new)
   Newspaper.subscribe(:create_user, Synchronizer::AssignedPullRequest.new)
+  Newspaper.subscribe(:create_user, Synchronizer::ReviewedPullRequest.new)
 end
