b5e5dd6d4a6c47f3c6c93d1259bf34799db2614e
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-31 02:11:57 +0900

    feat: GitHub API から取得した PullRequest をデータベースへ同期させる機能を追加

--- app/models/pull_request.rb
+++ app/models/pull_request.rb
@@ -12,6 +12,15 @@ class PullRequest < ApplicationRecord
   has_many :resolutions, dependent: :destroy
   has_many :issues, through: :resolutions, source: :issue
 
+  class << self
+    def synchronize(pull_requests_by_github_api)
+      hash_list = pull_requests_by_github_api.map(&:to_h)
+      upsert_all(hash_list) if hash_list.any?
+
+      Resolution.synchronize(pull_requests_by_github_api)
+    end
+  end
+
   def assignee?(user)
     assignees.include?(user)
   end
