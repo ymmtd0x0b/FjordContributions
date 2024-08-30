76b09d744ffb13801adc6d75da89f52aa60e2a17
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-08-31 05:23:57 +0900

    refactor: スペックのリファクタリング

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -17,40 +17,31 @@ RSpec.describe Issue, type: :model do
     context '引数の中に「未登録の Issue 」がある場合' do
       it '新たに登録すること' do
         create(:issue, repository_id: 123, id: 100, title: 'issue#100', number: 100, author_id: 1)
+        issues_collected_by_the_github_api = [
+          GitHub::Issue.new(repository_id: 123, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [],
+                                                         created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' }),
+          GitHub::Issue.new(repository_id: 123, issue: { id: 200, user_id: 1, title: 'issue#200', number: 200, labels_id: [],
+                                                         created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' })
+        ]
 
-        expect do
-          issues_collected_by_the_github_api = [
-            GitHub::Issue.new(repository_id: 123, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [],
-                                                           created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' }),
-            GitHub::Issue.new(repository_id: 123, issue: { id: 200, user_id: 1, title: 'issue#200', number: 200, labels_id: [],
-                                                           created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' })
-          ]
-          Issue.synchronize(issues_collected_by_the_github_api)
-        end.to change { Issue.count }.from(1).to(2)
+        expect { Issue.synchronize(issues_collected_by_the_github_api) }.to change { Issue.count }.from(1).to(2)
       end
     end
 
     context '引数の中に「登録済みの Issue 」がある場合' do
       it '該当 Issue の情報を更新すること' do
         issue = create(:issue, repository_id: 123, id: 100, title: 'before update...', number: 100, author_id: 1)
+        issues_collected_by_the_github_api = [
+          GitHub::Issue.new(repository_id: 123, issue: { id: 100, user_id: 1, title: 'updated!', number: 100, labels_id: [],
+                                                         created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' })
+        ]
 
-        expect do
-          issues_collected_by_the_github_api = [
-            GitHub::Issue.new(repository_id: 123, issue: { id: 100, user_id: 1, title: 'updated!', number: 100, labels_id: [],
-                                                           created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' })
-          ]
-          Issue.synchronize(issues_collected_by_the_github_api)
-        end.to change { issue.reload.title }.from('before update...').to('updated!')
+        expect { Issue.synchronize(issues_collected_by_the_github_api) }.to change { issue.reload.title }.from('before update...').to('updated!')
       end
     end
 
     it 'Issue のラベリングを同期させる(メソッドを呼び出す)こと' do
-      issues_collected_by_the_github_api = [
-        GitHub::Issue.new(repository_id: 123, issue: { id: 100, user_id: 1, title: 'issue#100', number: 100, labels_id: [123],
-                                                       created_at: '2000-01-01 09:00:00', updated_at: '2000-01-01 09:00:00' })
-      ]
-      Issue.synchronize(issues_collected_by_the_github_api)
-
+      Issue.synchronize([])
       expect(Labeling).to have_received(:synchronize)
     end
   end
--- spec/models/labeling_spec.rb
+++ spec/models/labeling_spec.rb
@@ -16,10 +16,9 @@ RSpec.describe Labeling, type: :model do
         issue = create(:issue, :with_author, repository_id: 123, id: 100)
         issue.labelings.create!(label_id: 10)
 
-        expect do
-          issues_collected_by_the_github_api = [GitHub::Issue.new(repository_id: 123, issue: { id: 100, labels_id: [10, 20] })]
-          Labeling.synchronize(issues_collected_by_the_github_api)
-        end.to change { issue.labels.ids }.from([10]).to([10, 20])
+        issues_collected_by_the_github_api = [GitHub::Issue.new(repository_id: 123, issue: { id: 100, labels_id: [10, 20] })]
+
+        expect { Labeling.synchronize(issues_collected_by_the_github_api) }.to change { issue.labels.ids }.from([10]).to([10, 20])
       end
     end
 
@@ -29,10 +28,9 @@ RSpec.describe Labeling, type: :model do
         issue.labelings.create!(label_id: 10)
         issue.labelings.create!(label_id: 20)
 
-        expect do
-          issues_collected_by_the_github_api = [GitHub::Issue.new(repository_id: 123, issue: { id: 100, labels_id: [10] })]
-          Labeling.synchronize(issues_collected_by_the_github_api)
-        end.to change { issue.labels.ids }.from([10, 20]).to([10])
+        issues_collected_by_the_github_api = [GitHub::Issue.new(repository_id: 123, issue: { id: 100, labels_id: [10] })]
+
+        expect { Labeling.synchronize(issues_collected_by_the_github_api) }.to change { issue.labels.ids }.from([10, 20]).to([10])
       end
     end
   end
