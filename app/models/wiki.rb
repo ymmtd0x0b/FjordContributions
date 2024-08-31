7b6a71c47e1dc68fc20336eaa736071f5b846ad6
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-01 02:51:49 +0900

    feat: Git clone から取得した Wiki をデータベースへ同期させる機能を追加

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -3,4 +3,15 @@
 class Wiki < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :repository
+
+  class << self
+    def synchronize(wikis_by_git, user)
+      user.wikis
+          .where.not(first_commit_hash: wikis_by_git.map(&:first_commit_hash))
+          .delete_all
+
+      hash_list = wikis_by_git.map(&:to_h)
+      upsert_all(hash_list, unique_by: %i[repository_id first_commit_hash]) if hash_list.any?
+    end
+  end
 end
