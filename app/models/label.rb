327b2b40616382bc5755c13719da83e3b5a8bd4a
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-02 06:46:39 +0900

    feat: GitHub API から取得した Label をデータベースへ同期させる機能を追加

--- app/models/label.rb
+++ app/models/label.rb
@@ -3,4 +3,14 @@
 class Label < ApplicationRecord
   belongs_to :repository
   has_many :labelings, dependent: :destroy
+
+  class << self
+    def synchronize(repository, labels_by_github_api)
+      labels_id = labels_by_github_api.map(&:id)
+      repository.labels.where.not(id: labels_id).delete_all
+
+      hash_list = labels_by_github_api.map(&:to_h)
+      upsert_all(hash_list) if hash_list.any?
+    end
+  end
 end
