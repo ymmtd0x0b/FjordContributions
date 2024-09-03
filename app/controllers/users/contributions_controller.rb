9d1af2e516abdfb05a38f39e10762baf0df07726
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-03 10:56:45 +0900

    chore: 不要なコントローラの共通処理を削除

--- app/controllers/users/contributions_controller.rb
+++ app/controllers/users/contributions_controller.rb
@@ -4,9 +4,9 @@ class Users::ContributionsController < ApplicationController
   include Settable
   skip_before_action :authenticate_user!
   before_action :set_repository, only: %i[index]
-  before_action :set_user, only: %i[index]
 
   def index
+    @user = User.find_by(login: params[:user_login])
     @issues = @user.issues.order(:created_at)
     @assigned_issues = @user.assigned_issues.order(:created_at)
     @reviewed_issues = @user.reviewed_issues.order(:created_at)
--- app/controllers/users/issues_controller.rb
+++ app/controllers/users/issues_controller.rb
@@ -3,17 +3,16 @@
 class Users::IssuesController < ApplicationController
   include Settable
   before_action :set_repository, only: %i[index]
-  before_action :set_user, only: %i[index]
 
   def index
     @issues =
       case params[:association]
       when 'assigned'
-        @user.assigned_issues.order(:created_at)
+        current_user.assigned_issues.order(:created_at)
       when 'reviewed'
-        @user.reviewed_issues.order(:created_at)
+        current_user.reviewed_issues.order(:created_at)
       else
-        @user.issues.order(:created_at)
+        current_user.issues.order(:created_at)
       end
   end
 end
