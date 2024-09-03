44cb0aab4e15a337479462431decf59c60072a72
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-04 01:00:55 +0900

    feat: Userモデルの has_many :issues を拡張して、他のユーザーがアサインしていない Issue を絞り込む機能を追加

--- app/models/concerns/user/issues_association_extension.rb
+++ app/models/concerns/user/issues_association_extension.rb
@@ -3,7 +3,8 @@
 module User::IssuesAssociationExtension
   def not_referenced_by_other_users
     sql = <<~"SQL"
-      #{not_created_by_other_users}
+      #{not_created_by_other_users}  AND
+      #{not_assigned_by_other_users}
     SQL
     where(sql, owner_id: proxy_association.owner.id)
   end
@@ -16,6 +17,16 @@ module User::IssuesAssociationExtension
         SELECT 1
         FROM users
         WHERE issues.author_id = users.id AND issues.author_id != :owner_id
+      )
+    SQL
+  end
+
+  def not_assigned_by_other_users
+    <<~SQL
+      NOT EXISTS (
+        SELECT 1
+        FROM assigns
+        WHERE assignable_type = 'Issue' AND assigns.assignable_id = issues.id AND assigns.user_id != :owner_id
       )
     SQL
   end
--- spec/models/user/issues_association_extention_spec.rb
+++ spec/models/user/issues_association_extention_spec.rb
@@ -9,9 +9,19 @@ RSpec.describe User::IssuesAssociationExtension do
       let(:taro) { create(:user, id: 456, login: 'taro') }
       let(:jiro) { create(:user, id: 789, login: 'jiro') }
 
+      # 以下は「他のユーザーが参照していない Issue を返すこと」を個別にテストしている
+
       it '他のユーザーが作成者でない Issue を返すこと' do
         create(:issue, repository:, id: 100, author: taro)
         create(:issue, repository:, id: 200, author: jiro)
+
+        actual = taro.issues.not_referenced_by_other_users.ids
+        expect(actual).to eq [100]
+      end
+
+      it '他のユーザーがアサインしていない Issue を返すこと' do
+        create(:issue, repository:, id: 100, author: taro)
+        create(:issue, repository:, id: 200, author: taro) { |issue| issue.assignees << jiro }
 
         actual = taro.issues.not_referenced_by_other_users.ids
         expect(actual).to eq [100]
