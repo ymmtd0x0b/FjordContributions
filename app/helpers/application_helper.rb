0cf5e45633f7b27f5d981803c97e509e4962b44c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-06 03:05:15 +0900

    chore: Toat の微修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def flash_bg_classes_for(flash_type)
-    case flash_type.to_s
-    when 'notice' then 'bg-green-500'
-    when 'alert'  then 'bg-red-500'
-    end
-  end
-
-  def flash_icon_classes_for(flash_type)
-    case flash_type.to_s
-    when 'notice' then 'text-green-500'
-    when 'alert'  then 'text-red-500'
-    end
-  end
 end
