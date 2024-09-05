c7affeefec08184b6a0f935cbd2c03b5842988d6
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-06 03:17:07 +0900

    chore: タブの切り替え機能を Stimulus から バックエンド側へ移譲

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def tab_to(name, path)
+    if path == request.fullpath
+      link_to name, path, class: 'inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active', aria: { current: 'page' }
+    else
+      link_to name, path, class: 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
+    end
+  end
 end
