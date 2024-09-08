250edde87b35fee30c90658e6498dacb7a5b082f
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-08 10:34:10 +0900

    chore: ルーティングの見直し(ユーザーの操作は本人以外には許可しないので全て current_user に移譲する)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'auth/github/callback', to: 'user_sessions#create'
   delete 'logout', to: 'user_sessions#destroy'
+  resource :current_user, only: %i[update destroy], controller: 'current_user'
   namespace :current_user do
     resources :issues, only: %i[index]
     resources :wikis, only: %i[index]
@@ -9,5 +10,4 @@ Rails.application.routes.draw do
   namespace :users, path: '/:user_login' do
     resources :contributions, only: %i[index]
   end
-  resources :users, only: %i[update destroy]
 end
