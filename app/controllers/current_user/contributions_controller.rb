15e1754e693d33a2430a39691091222f4ad35c72
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-13 11:02:38 +0900

    chore: Bullet による N+1 の指摘に対応

--- app/controllers/current_user/contributions_controller.rb
+++ app/controllers/current_user/contributions_controller.rb
@@ -4,9 +4,9 @@ class CurrentUser::ContributionsController < ApplicationController
   before_action :set_repository, only: %i[index]
 
   def index
-    @issues = current_user.issues.order(:created_at)
-    @assigned_issues = current_user.assigned_issues.order(:created_at)
-    @reviewed_issues = current_user.reviewed_issues.order(:created_at)
-    @wikis = current_user.wikis.order(:created_at)
+    @issues = current_user.issues.includes(:repository).order(:created_at)
+    @assigned_issues = current_user.assigned_issues.includes(%i[repository pull_requests]).order(:created_at)
+    @reviewed_issues = current_user.reviewed_issues.includes(%i[repository pull_requests]).order(:created_at)
+    @wikis = current_user.wikis.includes(:repository).order(:created_at)
   end
 end
