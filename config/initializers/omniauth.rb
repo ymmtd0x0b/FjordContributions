40004360695817d3e4b67b5f0d8f0d73c9153bd8
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-14 13:44:43 +0900

    chore: デザインレビュー向けに GitHub 認証のモックを設定

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,3 +5,19 @@ end
 OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
 }
+
+# デザインレビュー用の設定
+# レビューが終わり次第削除する
+if Rails.env.production?
+  OmniAuth.config.test_mode = true
+  OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
+    provider: 'github',
+    uid: 95903475,
+    info: {
+      nickname: 'ymmtd0x0b',
+      name: 'ヤマモト',
+      image: 'https://avatars.githubusercontent.com/u/95903475?v=4'
+    }
+  )
+end
+# ここまで
