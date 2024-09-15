2cd075e189cc4dc1f9abf4052002e110f3bf6a7d
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-15 15:55:26 +0900

    refactor: spec/decorator のリファクタリング

--- spec/decorators/wiki_decorator_rspec.rb
+++ spec/decorators/wiki_decorator_rspec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe WikiDecorator do
   describe '#url' do
-    it '「環境変数(dotenv)に設定したGitHubのURL + リポジトリ名 + wiki + Wikiのタイトル」に変換した URL を返すこと' do
+    it '「環境変数に設定したGitHubのURL/リポジトリ名/wiki/Wikiのタイトル」に変換した URL を返すこと' do
       create(:repository, id: 123, name: 'test/repository')
       wiki = create(:wiki, :with_author, repository_id: 123, title: '議事録#001')
       decorator_wiki = ActiveDecorator::Decorator.instance.decorate(wiki)
