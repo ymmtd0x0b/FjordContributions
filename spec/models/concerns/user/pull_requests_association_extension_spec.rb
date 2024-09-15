25a2dc64207982221eb679ed99e15252f136c1f4
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-15 16:28:41 +0900

    refactor: モデルの拡張アソシエーションのスペックをリファクタリング

--- spec/models/concerns/user/pull_requests_association_extension_spec.rb
+++ spec/models/concerns/user/pull_requests_association_extension_spec.rb
@@ -12,20 +12,16 @@ RSpec.describe User::PullRequestsAssociationExtension do
       # 以下は「他のユーザーが参照していない PullRequest を返すこと」を個別にテストしている
 
       it '他のユーザーがアサインしていない PullRequest を返すこと' do
-        taro.assigned_pull_requests << [
-          create(:pull_request, repository:, id: 100),
-          create(:pull_request, repository:, id: 200) { |pr| pr.assignees << jiro }
-        ]
+        taro.assigned_pull_requests << create(:pull_request, repository:, id: 100)
+        taro.assigned_pull_requests << create(:pull_request, repository:, id: 200) { |pr| pr.assignees << jiro }
 
         actual = taro.assigned_pull_requests.not_referenced_by_other_users.ids
         expect(actual).to eq [100]
       end
 
       it '他のユーザーがレビューしていない PullRequest を返すこと' do
-        taro.assigned_pull_requests << [
-          create(:pull_request, repository:, id: 100),
-          create(:pull_request, repository:, id: 200) { |pr| pr.reviewers << jiro }
-        ]
+        taro.assigned_pull_requests << create(:pull_request, repository:, id: 100)
+        taro.assigned_pull_requests << create(:pull_request, repository:, id: 200) { |pr| pr.reviewers << jiro }
 
         actual = taro.assigned_pull_requests.not_referenced_by_other_users.ids
         expect(actual).to eq [100]
