9a09b0cd13a800747c696b0f8785542c7ce35e7c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-16 00:19:03 +0900

    chore: 初期データ投入方法を第三者向けに更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,79 +1,11 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+repository_id = ENV['BOOTCAMP_REPOSITORY_ID']
+return if Repository.exists?(id: repository_id)
 
-# 既存のデータを削除
-Repository.destroy_all
-Label.destroy_all
-User.destroy_all
-Issue.destroy_all
-Assign.destroy_all
-PullRequest.destroy_all
-Review.destroy_all
-Resolution.destroy_all
-Wiki.destroy_all
-Labeling.destroy_all
-
-# 初期データの投入
-repo_by_api = GitHub::Repository.find_by(id: ENV['BOOTCAMP_REPOSITORY_ID'])
-Repository.create!(repo_by_api.to_h)
-
-# labels_by_api = GitHub::Label.registered_by(repository)
-# labels_by_api.each { |label| Label.create!(label.to_h) }
-
-# user_by_api = GitHub::User.find_by(login: ENV['USER_LOGIN'])
-# User.create!(user_by_api.to_h)
-
-# created_issues = GitHub::Issue.created_by(repository, user)
-# created_issues.each do |issue|
-#   Issue.create!(issue.to_h)
-#   issue.create_labelings.each { |labeling| Labeling.create!(labeling) }
-# end
-
-# assigned_issues = GitHub::Issue.assigned_by(repository, user)
-# assigned_issues.each do |issue|
-#   unless Issue.find_by(id: issue.id)
-#     Issue.create!(issue.to_h)
-#     issue.create_labelings.each { |labeling| Labeling.create!(labeling) }
-#   end
-#   Assign.create!(assignable_type: 'Issue', assignable_id: issue.id, user:)
-# end
-
-# assigned_pull_requests = GitHub::PullRequest.assigned_by(repository, user)
-# assigned_pull_requests.each do |pull_request|
-#   PullRequest.create!(pull_request.to_h)
-#   Assign.create!(assignable_type: 'PullRequest', assignable_id: pull_request.id, user:)
-#   pull_request.resolutions.each do |resolution|
-#     Resolution.create!(resolution) unless Resolution.find_by(resolution)
-#   end
-# end
-
-# reviewed_issues = GitHub::Issue.reviewed_by(repository, user)
-# reviewed_issues.each do |issue|
-#   unless Issue.find_by(id: issue.id)
-#     Issue.create!(issue.to_h)
-#     issue.create_labelings.each { |labeling| Labeling.create!(labeling) }
-#   end
-# end
-
-# reviews_pull_requests = GitHub::PullRequest.reviewed_by(repository, user)
-# reviews_pull_requests.each do |pull_request|
-#   PullRequest.create!(pull_request.to_h)
-#   Review.create!(pull_request_id: pull_request.id, user:)
-#   pull_request.resolutions.each do |resolution|
-#     Resolution.create!(resolution) unless Resolution.find_by(resolution)
-#   end
-# end
-
-# created_wikis = Git::Wiki.created_by(repository, user)
-# created_wikis.each do |wiki|
-#   Wiki.create!(wiki.to_h)
-# end
+repo_data = GitHub::Repository.find_by(id: repository_id)
+if repo_data
+  Repository.create!(repo_data.to_h)
+else
+  puts 'データの取得に失敗しました。詳細は /log/development.log を確認してください。' # rubocop:disable Rails/Output
+end
