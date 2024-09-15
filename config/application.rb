a828fee012acf5337b22687e1d7a329b0910da60
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-15 15:40:25 +0900

    test: テストの追加 (helper.tab_to)

--- config/application.rb
+++ config/application.rb
@@ -44,7 +44,7 @@ module FjordContributions
     config.generators do |g|
       g.test_framework :rspec,
         view_specs: false,
-        helper_specs: false,
+        # helper_specs: false,
         routing_specs: false
     end
   end
