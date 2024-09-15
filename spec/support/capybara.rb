710d73b5e6c82341f2e08c03ea0082f7163d6a1c
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-16 00:17:57 +0900

    chore: 通常ブラウザでのテスト実行オプションを適切な表現に変更

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  if ENV['HEAD']
+  if ENV['HEADFULL']
     config.before(:each, type: :system) { driven_by :selenium_chrome }
   else
     config.before(:each, type: :system) { driven_by :selenium_chrome_headless }
