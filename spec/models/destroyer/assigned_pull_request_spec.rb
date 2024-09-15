01142339b4e3394550e7989e1bf23d0026951d38
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-15 17:48:48 +0900

    refactor: ユーザーに関連する Issue / PullRequest を削除するモデルのテストをリファクタリング

--- spec/models/destroyer/assigned_pull_request_spec.rb
+++ spec/models/destroyer/assigned_pull_request_spec.rb
@@ -10,13 +10,14 @@ RSpec.describe Destroyer::AssignedPullRequest, type: :model do
 
     it '他のユーザーから参照されていない「ユーザーがアサインしている PullRequest 」を削除すること' do
       taro.assigned_pull_requests << [
-        create(:pull_request, repository:),
-        create(:pull_request, repository:) { |pr| pr.assignees << jiro },
-        create(:pull_request, repository:) { |pr| pr.reviewers << jiro }
+        create(:pull_request, id: 1, repository:),
+        create(:pull_request, id: 2, repository:) { |pr| pr.assignees << jiro },
+        create(:pull_request, id: 3, repository:) { |pr| pr.reviewers << jiro }
       ]
 
       destroyer = Destroyer::AssignedPullRequest.new
-      expect { destroyer.call(taro) }.to change { taro.assigned_pull_requests.count }.from(3).to(2)
+      expect { destroyer.call(taro) }.to change { taro.assigned_pull_requests.count }.by(-1)
+                                     .and change { PullRequest.exists?(id: 1) }.from(true).to(false)
     end
   end
 end
--- spec/models/destroyer/created_issue_spec.rb
+++ spec/models/destroyer/created_issue_spec.rb
@@ -12,12 +12,13 @@ RSpec.describe Destroyer::CreatedIssue, type: :model do
     let(:jiro_reviewed_pr) { create(:pull_request, repository:) { |pr| pr.reviewers << jiro } }
 
     it '他のユーザーから参照されていない「ユーザーが作成者である Issue 」を削除すること' do
-      create(:issue, repository:, author: taro)
-      create(:issue, repository:, author: taro) { |issue| issue.assignees << jiro }
-      create(:issue, repository:, author: taro) { |issue| issue.pull_requests << jiro_assigned_pr }
-      create(:issue, repository:, author: taro) { |issue| issue.pull_requests << jiro_reviewed_pr }
+      create(:issue, id: 1, repository:, author: taro)
+      create(:issue, id: 2, repository:, author: taro) { |issue| issue.assignees << jiro }
+      create(:issue, id: 3, repository:, author: taro) { |issue| issue.pull_requests << jiro_assigned_pr }
+      create(:issue, id: 4, repository:, author: taro) { |issue| issue.pull_requests << jiro_reviewed_pr }
 
-      expect { destroyer.call(taro) }.to change { taro.issues.count }.from(4).to(3)
+      expect { destroyer.call(taro) }.to change { taro.issues.count }.by(-1)
+                                     .and change { Issue.exists?(id: 1) }.from(true).to(false)
     end
   end
 end
