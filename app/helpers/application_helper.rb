c39a3e3723634c3d6e0ba4caac00c2b78e197d72
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-18 01:53:37 +0900

    chore: ユーザーの取り組み一覧を Turbo Frames 化 (同時に Toast の Stimulus#connect エラーにも対処)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,11 @@
 module ApplicationHelper
   def tab_to(name, path)
     if path == request.fullpath
-      link_to name, path, class: 'inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active', aria: { current: 'page' }
+      link_to name, path, data: { turbo_action: :advance },
+                          class: 'inline-block p-4 text-blue-600 border-b-2 border-blue-600 rounded-t-lg active', aria: { current: 'page' }
     else
-      link_to name, path, class: 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
+      link_to name, path, data: { turbo_action: :advance },
+                          class: 'inline-block p-4 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300'
     end
   end
 end
