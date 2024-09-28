c58c79387afe00ad91fe911036db7c2f7f1dfbfc
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-09-29 04:28:36 +0900

    chore: ユーザー登録・更新・削除する際のデータ処理にトランザクションを設定

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -4,21 +4,27 @@ class CurrentUserController < ApplicationController
   before_action :set_repository, only: %i[update]
 
   def update
-    latest_user_data = GitHub::User.find_by(id: current_user.id)
-    if latest_user_data && current_user.update!(latest_user_data.to_h)
+    ActiveRecord::Base.transaction do
+      latest_user_data = GitHub::User.find_by(id: current_user.id)
+      current_user.update!(latest_user_data.to_h)
       Newspaper.publish(:user_update, { repository: @repository, user: current_user })
       flash[:notice] = '更新に成功しました'
-    else
-      flash[:alert] = '更新に失敗しました'
+      redirect_back fallback_location: root_path
     end
-
-    redirect_to request.headers[:HTTP_REFERER]
+  rescue Octokit::Error, ActiveRecord::StatementInvalid
+    flash[:alert] = '更新に失敗しました'
+    redirect_back fallback_location: root_path
   end
 
   def destroy
-    Newspaper.publish(:user_destroy, current_user)
-    current_user.destroy!
+    ActiveRecord::Base.transaction do
+      Newspaper.publish(:user_destroy, current_user)
+      current_user.destroy!
+    end
     reset_session
     redirect_to root_path, notice: 'アカウントを削除しました', status: :see_other
+  rescue ActiveRecord::RecordNotDestroyed
+    flash[:alert] = '削除に失敗しました'
+    redirect_back fallback_location: root_path
   end
 end
