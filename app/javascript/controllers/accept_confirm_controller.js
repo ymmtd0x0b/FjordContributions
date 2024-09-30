f95e5b30f5d3bfd505a75b0cae8186d3f8ab155b
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-01 05:24:40 +0900

    style: リンターの指摘を修正

--- app/javascript/controllers/accept_confirm_controller.js
+++ app/javascript/controllers/accept_confirm_controller.js
@@ -1,12 +1,12 @@
-import { Controller } from '@hotwired/stimulus'
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ['modal', 'dialog', 'loading']
+  static targets = ["modal", "dialog", "loading"];
 
   showLoading() {
-    this.dialogTarget.classList.add('hidden')
-    this.loadingTarget.classList.remove('hidden')
-    this.modalTarget.classList.add('pointer-events-none')
+    this.dialogTarget.classList.add("hidden");
+    this.loadingTarget.classList.remove("hidden");
+    this.modalTarget.classList.add("pointer-events-none");
   }
 }
--- app/javascript/helpers/switching_show_hidden.js
+++ app/javascript/helpers/switching_show_hidden.js
@@ -1,10 +1,10 @@
 export const switchingShowHidden = (showEl, hideEl) => {
-  showEl.classList.add('hidden')
-  hideEl.classList.remove('hidden')
+  showEl.classList.add("hidden");
+  hideEl.classList.remove("hidden");
 
   // reset to default state
   setTimeout(() => {
-    showEl.classList.remove('hidden')
-    hideEl.classList.add('hidden')
-  }, 2000)
-}
+    showEl.classList.remove("hidden");
+    hideEl.classList.add("hidden");
+  }, 2000);
+};
