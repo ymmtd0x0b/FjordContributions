9b58ffc93895494a40862887e9e4dce34e95e0a9
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-01 11:55:21 +0900

    refactor: N+1 対策用の includes を削除し、eager_load と preload で置き換える

--- app/controllers/current_user/wikis_controller.rb
+++ app/controllers/current_user/wikis_controller.rb
@@ -4,6 +4,6 @@ class CurrentUser::WikisController < ApplicationController
   before_action :set_repository, only: %i[index]
 
   def index
-    @wikis = current_user.wikis.includes(:repository).order(:created_at)
+    @wikis = current_user.wikis.eager_load(:repository).order(:created_at)
   end
 end
