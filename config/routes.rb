e1d7133a15836246ecd40adcec041125306c9977
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-01 12:58:26 +0900

    refactor: current_user/issues, wikis, contributions の設定方法を変更 (routes.rb)

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   get 'auth/github/callback', to: 'user_sessions#create'
   get 'auth/failure', to: 'user_sessions#failure'
   delete 'logout', to: 'user_sessions#destroy'
-  resource :current_user, only: %i[update destroy]
-  namespace :current_user do
-    resources :issues, only: %i[index]
-    resources :wikis, only: %i[index]
-    resources :contributions, only: %i[index]
+  resource :current_user, only: %i[update destroy] do
+    resources :issues, only: %i[index], controller: 'current_user/issues'
+    resources :wikis, only: %i[index], controller: 'current_user/wikis'
+    resources :contributions, only: %i[index], controller: 'current_user/contributions'
   end
   resources :users, param: :login, only: [] do
     resources :contributions, only: %i[index], controller: 'users/contributions'
