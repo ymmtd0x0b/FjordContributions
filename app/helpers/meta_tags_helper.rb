1f61a04d490c0a203781220ddffaf5157a156352
ymmtd0x0b <ymmtd0x0b@gmail.com>
2024-10-08 15:23:49 +0900

    chore: metaタグの description をトップページに合わせた

--- app/helpers/meta_tags_helper.rb
+++ app/helpers/meta_tags_helper.rb
@@ -7,7 +7,7 @@ module MetaTagsHelper
     {
       site: 'FjordContributions',
       charset: 'utf-8',
-      description: 'チーム開発プラクティスでの GitHub 上の取り組みを個人に焦点を当てて可視化できるサービスです。',
+      description: 'フィヨルドブートキャンプのチーム開発プラクティスで、あなたが関わった Issue や PullRequest を自動で取得して一覧表示できるサービスです。',
       keywords: 'GitHub,Issue,PullRequest,チーム開発プラクティス,フィヨルドブートキャンプ,FBC',
       reverse: true,
       og: {
